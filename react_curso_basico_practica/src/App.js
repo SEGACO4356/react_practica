f70746f53e3753729420553d5ed6137da05fcdab
Sebastián García Correa <sebastian435622@gmail.com>
2023-02-20 10:41:56 -0500

    Avanzando useState

--- react_curso_basico_practica/src/App.js
+++ react_curso_basico_practica/src/App.js
@@ -2,26 +2,37 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [n1, setN1] = useState();
-  const [n2, setN2] = useState();
-  const [resultado, setResultado] = useState();
 
-  const sumar = () => {
-    setResultado(Number(n1) + Number(n2));
-  };
-  const modificar =(e)=>{
-    setN1(e.target.value)
+  const rey=[
+    {
+    nombre: "Pirri",
+    aficion: "Sisa",
+    },{
+      nombre: "nombre2",
+      aficion: "nosesabe2",
+    },{
+      nombre: 
+      "nombre3",
+      aficion: "nosesabe3",
+    }
+  ]
+  const [contador, setContador] = useState(0);
+  const [mensaje, SetMensaje] = useState();
+ const cambio = ()=>{
+  setContador(contador+1);
+  if (contador+1 >= rey.length) {
+      setContador(0);
   }
-  const modificar2 =(e)=>{
-    setN2(e.target.value)
-  }
+  SetMensaje(<h2>Solo <span className="span">{rey[contador].nombre}</span>
+  Si <span className="span2">{rey[contador].aficion}</span></h2>)
+ }
+
+
   return (
-    <div className="caja">
-      <input type="number" value={n1} onChange={modificar} />+
-      <input type="number" value={n2} onChange={modificar2}/>=
-      <input type="number" value={resultado}readOnly />
-      <button onClick={sumar}>Sumar</button>
-    </div>
+   <>
+   <button onClick={cambio}>Pase</button>
+   <div>{mensaje}</div>
+   </>
   );
 };
 
